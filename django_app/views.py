53374466920e07fa2e50fa79d7897600009ae21c
Lr1808 <rodulfoluis1808@gmail.com>
2025-10-23 17:53:17 -0700

    create messege board homepege (View+template+URLs)

--- django_app/views.py
+++ django_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Posr
 
 # Create your views here.
+class HomeView(ListView):
+    model = Posr
+    template_name = "home.html"
+    context_object_name = "all_posts_list"
